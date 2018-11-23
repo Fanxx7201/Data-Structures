9af35fa049a3b626bb760b2a16f09c937409483d
fxx <fanxx_3516@163.com>
2018-11-23 09:09:41 +0800

    测试代码

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -3,6 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<>();
-        int[] nums = {};
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num: nums){
+            bst.add(num);
+        }
+
+        bst.preOrder();
+        System.out.println();
+
+        System.out.println(bst);
+
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
     }
 }
