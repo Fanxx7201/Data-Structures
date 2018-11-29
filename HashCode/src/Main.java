6df65c4ee93c11ae6c8a449a91628766ddef8728
fxx <fanxx_3516@163.com>
2018-11-29 17:21:14 +0800

    hashCode

--- HashCode/src/Main.java
+++ HashCode/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.HashSet;
+import java.util.HashMap;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Student student = new Student(2, 2, "bobo", "liu");
+        System.out.println(student.hashCode());
+
+        //这样, 我们就可以使用java给我们提供的Hash表
+        //以下就是java提供的hash表的基本使用方法.
+        HashSet<Student> set = new HashSet<>();
+        set.add(student);
+
+        HashMap<Student, Integer> scores = new HashMap<>();
+        scores.put(student, 100);
+
     }
 }
